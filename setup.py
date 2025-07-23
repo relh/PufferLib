075f141f83000058b7120256b1968b43a3b85e32
Richard Higgins <richard@softmax.com>
2025-07-23 15:21:45 -0700

    just install metta

--- setup.py
+++ setup.py
@@ -252,10 +252,9 @@ environments = {
         'numpy<2',
     ],
     'metta': [
-        'pufferlib[metta-deps]', # please preserve the metta-deps to metta 1-2 punch because of --no-build_isolation
-        # Install metta-mettagrid directly from the subdirectory
-        'metta-mettagrid @ git+https://github.com/metta-ai/metta.git@richard-alt-versions#subdirectory=mettagrid',
-        'metta-common @ git+https://github.com/metta-ai/metta.git@richard-alt-versions#subdirectory=common',
+        'pufferlib[metta-deps]',
+        # Install metta and all its subpackages from the root
+        'metta @ git+https://github.com/metta-ai/metta.git@richard-alt-versions',
         f'gym=={GYM_VERSION}',
         f'gymnasium=={GYMNASIUM_VERSION}',
         'omegaconf',
